bb15f9ac831e97f2c964881729461e2839019b2f
m.tarkhan <m.tarkhan@refahbank.ir>
2023-06-26 12:08:11 +0330

    the first sample of threading

---
+++ SimpleThreadPartOne/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// the name of each thread (if it is not a pooled one) can be change only once before start
+Thread.CurrentThread.Name = "A";
+Console.WriteLine($"{Thread.CurrentThread.Name} is running...");
+
+Thread writeBThread = new Thread(WriteB);
+writeBThread.Name = "B";
+
+// this will invoke the WriteB method but concurrent to CurrentThread (A)
+writeBThread.Start();
+
+for ( int i = 0; i < 1000; i++) Console.Write(Thread.CurrentThread.Name);
+
+// waits A until B finishes
+writeBThread.Join();
+
+Console.WriteLine("\nThe process has finished.");
+Console.ReadLine();
+
+void WriteB()
+{
+    for (int i = 0; i < 1100; i++) Console.Write(Thread.CurrentThread.Name);
+}
+
